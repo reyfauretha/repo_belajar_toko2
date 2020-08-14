ab36110c191fc7cf8979f071285a8bf5e82ab71b
reyfauretha <reyfa_auretha_28rpl@student.smktelkom-mlg.sch.id>
2020-08-14 19:53:33 +0700

    Migrasi database db_toko

---
+++ database/migrations/2020_08_14_084047_create_transaksi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->bigIncrements('id_transaksi');
+            $table->integer('total_harga');
+            $table->unsignedBigInteger('id_orders');
+
+            $table->foreign('id_orders')->references('id_orders')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
