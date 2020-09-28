7e30b8e426f73b54b89d36d1319a9453aba68a37
reyfauretha <reyfa_auretha_28rpl@student.smktelkom-mlg.sch.id>
2020-09-28 21:29:38 +0700

    menambah autensikasi

--- routes/api.php
+++ routes/api.php
@@ -12,29 +12,32 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register','UserController@register');
+Route::post('/login', 'UserController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['jwt.verify']], function ()
+{
+
+    Route::get('/customers', 'CustomersController@show');
+    Route::post('/customers','CustomersController@store');
+    Route::put('/customers/{id}', 'CustomersController@update');
+    Route::delete('/customers/{id}', 'CustomersController@destroy');
+
+    Route::get('/product', 'productController@show');
+    Route::post('/product','ProductController@store');
+    Route::put('/product/{id}', 'ProductController@update');
+    Route::delete('/product/{id}', 'ProductController@destroy');
+
+    Route::get('/orders', 'OrdersController@show');
+    Route::get('/orders/{id}', 'OrdersController@detail');
+    Route::post('/orders','OrdersController@store');
+    Route::put('/orders/{id}', 'OrdersController@update');
+    Route::delete('/orders/{id}', 'OrdersController@destroy');
+
+    Route::get('/transaksi', 'TransaksiController@show');
+    Route::get('/transaksi/{id}', 'TransaksiController@detail');
+    Route::post('/transaksi','TransaksiController@store');
+    Route::put('/transaksi/{id}', 'TransaksiController@update');
+    Route::delete('/transaksi/{id}', 'TransaksiController@destroy');
+
 });
-
-Route::get('/customers', 'CustomersController@show');
-Route::post('/customers','CustomersController@store');
-Route::put('/customers/{id}', 'CustomersController@update');
-Route::delete('/customers/{id}', 'CustomersController@destroy');
-
-Route::get('/product', 'productController@show');
-Route::post('/product','ProductController@store');
-Route::put('/product/{id}', 'ProductController@update');
-Route::delete('/product/{id}', 'ProductController@destroy');
-
-Route::get('/orders', 'OrdersController@show');
-Route::get('/orders/{id}', 'OrdersController@detail');
-Route::post('/orders','OrdersController@store');
-Route::put('/orders/{id}', 'OrdersController@update');
-Route::delete('/orders/{id}', 'OrdersController@destroy');
-
-Route::get('/transaksi', 'TransaksiController@show');
-Route::get('/transaksi/{id}', 'TransaksiController@detail');
-Route::post('/transaksi','TransaksiController@store');
-Route::put('/transaksi/{id}', 'TransaksiController@update');
-Route::delete('/transaksi/{id}', 'TransaksiController@destroy');
